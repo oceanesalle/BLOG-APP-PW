81d33ca0f812be72b915fbb11f03c8e8c28e4942
oceansalle <oceanesalleiae@gmail.com>
2022-07-05 15:03:00 +0200

    navbar & home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,24 @@
-import React, { useState, useEffect } from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-
-import { getPosts } from './actions/posts.js';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import lac from './images/lac.jpg'
-import useStyles from './style';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import Navbar from './components/Navbar/Navbar.js';
+import Home from './components/Home/Home.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(0);
-  const classes = useStyles();
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
 
   return (
+    <BrowserRouter>
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">BLOG</Typography>
-        <img className={classes.image} src={lac} alt="icon" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/> 
+        <Route path="/auth" element={<Auth />}/>
+      </Routes>
+      <Home />
     </Container>
+    </BrowserRouter>
   );
 };
 export default App
